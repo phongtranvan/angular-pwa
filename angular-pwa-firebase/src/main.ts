248a325109168024ed6fa2b82ef4e3d22cab9705
Phong.Tran <phong.tran@evizi.com>
2024-10-28 15:56:18 +0700

    Initial

--- angular-pwa-firebase/src/main.ts
+++ angular-pwa-firebase/src/main.ts
@@ -7,6 +7,15 @@ import { provideMessaging, getMessaging } from '@angular/fire/messaging';
 import { provideServiceWorker, SwUpdate } from '@angular/service-worker';
 import { environment } from './environments/environment';
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/firebase-messaging-sw.js')
+    .then((registration) => {
+      console.log('Service Worker registered with scope:', registration.scope);
+    }).catch((error) => {
+      console.error('Service Worker registration failed:', error);
+    });
+}
+
 bootstrapApplication(AppComponent, {
   providers: [
     provideFirebaseApp(() => initializeApp(firebaseConfig)),
